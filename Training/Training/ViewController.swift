895482faba9e5750ec4147155352a47152a810da
秋庭圭吾 <akibakeigo@shinonome.local>
2022-07-26 11:02:19 +0900

    new

---
+++ Training/Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by 秋庭圭吾 on 2022/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
