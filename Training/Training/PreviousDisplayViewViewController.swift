daf2d4e768a3de698473487fee6060221d507326
秋庭圭吾 <akibakeigo@shinonome.local>
2022-08-03 16:35:21 +0900

    初期画面をダークグレーに変更 view表示仕切ったら画面遷移する仕様に変更

--- Training/Training/PreviousDisplayViewViewController.swift
+++ Training/Training/PreviousDisplayViewViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class PreviousDisplayViewViewController: UIViewController {
 
+    //viewを読み込んだ後に動く処理
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.darkGray
     }
     
+    //viewを表示しきった後に動く処理
+    override func viewDidAppear(_ animated: Bool) {
+        //storyboardIDで画面紐付け
+        let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "forecastScreen")
+        //nilチェックしてフルスクリーン設定と画面遷移処理
+        if let next = nextViewController {
+            next.modalPresentationStyle = .fullScreen
+            self.present(next, animated: true, completion: nil)
+        }
+    }
 
     /*
     // MARK: - Navigation
